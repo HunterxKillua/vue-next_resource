874877a1011cdf932bc6498d73cd8f09f1b8322e
wcj199621714@outlook.com <chengjie@wangchengjiedebijibendiannao.local>
2023-02-11 04:02:47 +0800

    feat: about reactive without array

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ const activeStack = [];
 const jobQueue = new Set();
 const p = Promise.resolve();
 let isFlushing = false;
+let ITERATE_KEY = Symbol();
 
 const flushJob = () => {
   if (isFlushing) return;
@@ -16,15 +17,60 @@ const flushJob = () => {
   })
 }
 
-const data = { text: 'hello world', ok: true, count: 1, num: 2 };
-const obj = new Proxy(data, {
-  get(target, key) {
-    track(target, key);
-    return target[key];
+const isShallowReactive = (source) => reactive(source, true)
+const readOnly = (source) => reactive(source, false, true)
+
+const reactive = (source, isShallow = false, isReadOnly) => new Proxy(source, {
+  get(target, key, receiver) {
+    if (key === 'raw') {
+      return target
+    }
+    if (!isReadOnly) {
+      track(target, key)
+    }
+    const res = Reflect.get(target, key, receiver);
+    if (isShallow) {
+      return res
+    }
+    if (typeof res === 'object' && res !== null) {
+      return isReadOnly ? readOnly(res) : reactive(res)
+    } 
+    return res
   },
-  set(target, key, newValue) {
-    target[key] = newValue;
-    trigger(target, key);
+  set(target, key, newValue, receiver) {
+    if (isReadOnly) {
+      console.warn(`属性 ${key} 是只读的`)
+      return true
+    }
+    const oldValue = target[key]
+    const type = Object.prototype.hasOwnProperty.call(target, key) ? 'SET' : 'ADD'
+    const res = Reflect.set(target, key, newValue, receiver)
+    if (target === receiver.raw) {
+      if (oldValue !== newValue && (oldValue === oldValue || newValue === newValue)) { // 后面条件为了排除NaN
+        trigger(target, key, type);
+      }
+    }
+    return res
+  },
+  deleteProperty(target, key) {
+    if (isReadOnly) {
+      console.warn(`属性 ${key} 是只读的`)
+      return true
+    }
+    const hadKey = Object.prototype.hasOwnProperty.call(target, key)
+    const res = Reflect.deleteProperty(target, key)
+    if (res && hadKey) {
+      trigger(target, key, 'DELETE')
+    }
+    return res
+  },
+  has(target, key) {
+    track(target, key)
+    return Reflect.has(target, key)
+  },
+  ownKeys(target) {
+    track(target, ITERATE_KEY)
+    return Reflect.ownKeys(target)
   }
 })
 
@@ -41,16 +87,24 @@ const track = (target, key) => {
   activeEffect.deps.push(deps)
 }
 
-const trigger = (target, key) => {
+const trigger = (target, key, type) => {
   const depsMap = bucket.get(target)
   if (!depsMap) return
   const effects = depsMap.get(key)
+  const iterateEffects = depsMap.get(ITERATE_KEY)
   const effectToRun = new Set()
   effects && effects.forEach(fn => {
     if (fn !== activeEffect) {
       effectToRun.add(fn)
     }
   })
+  if (type === 'ADD' || type === 'DELETE') {
+    iterateEffects && iterateEffects.forEach(fn => {
+      if (fn !== activeEffect) {
+        effectToRun.add(fn)
+      }
+    })
+  }
   effectToRun.forEach(fn => {
     if (fn.options.scheduler) {
       fn.options.scheduler(fn)
@@ -85,17 +139,6 @@ const effect = (fn, options = {}) => {
   }
   return effectFn;
 }
-
-let templ1, templ2
-
-effect(() => {
-  console.log(obj.count)
-}, {
-  scheduler(fn){
-    jobQueue.add(fn)
-    flushJob()
-  }
-})
 
 const computed = (getter) => {
   let value // 作为缓存
@@ -162,12 +205,24 @@ const watch = (source, callback, options = {}) => {
   }
 }
 
-const resp = computed(() => obj.count + 1)
+const data = { text: 'hello world', get print() {
+  return this.text
+}};
 
-watch(() => obj.text, (newValue, oldValue) => {
-  console.log(newValue, oldValue)
-}, {
-  immediate: true
+const obj = isShallowReactive({
+  foo: 'hello',
+  props: {
+    num: 1
+  }
 })
 
-obj.text = ''
+const deeps = readOnly({
+  foo: {
+    bar: '123'
+  }
+})
+
+effect(() => {
+  console.log(obj.foo)
+  console.log(deeps.foo.bar)
+})
